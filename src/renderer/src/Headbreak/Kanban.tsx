518c71024a3adb0f6d493b005f2f366d1468718e
Arastryx <arastryx.dravoga@gmail.com>
2026-09-01 15:45:25 -0300

    Add Columns
    -The kanban now starts empty and you can add columns to it

--- src/renderer/src/Headbreak/Kanban.tsx
+++ src/renderer/src/Headbreak/Kanban.tsx
@@ -1,20 +1,32 @@
 import React, { useState } from 'react'
 import { Column } from './Column'
-import { Box, Stack } from '@mui/material'
+import { Box, Button, Stack } from '@mui/material'
 import { DragDropProvider } from '@dnd-kit/react'
 import { useTasks } from './TaskProvider'
+import AddIcon from '@mui/icons-material/Add'
+
+let idCounter = 1
+
+interface Column {
+  id: number
+  label?: string
+}
 
 export interface KanbanProps {}
 
 export function Kanban({}: KanbanProps) {
   const { data: tasks } = useTasks()
+  const [columns, setColumns] = useState<Column[]>([])
 
   return (
     <DragDropProvider>
       <Stack direction={'row'} spacing={2} sx={{ height: '100vh', p: 2 }}>
-        <Column id={1} tasks={tasks?.[1]} />
-        <Column id={2} tasks={tasks?.[2]} />
-        <Column id={3} tasks={tasks?.[3]} />
+        {columns.map((c) => (
+          <Column key={c.id} id={c.id} tasks={tasks?.[c.id]} />
+        ))}
+        <Button onClick={() => setColumns([...columns, { id: idCounter++ }])}>
+          <AddIcon />
+        </Button>
       </Stack>
     </DragDropProvider>
   )
